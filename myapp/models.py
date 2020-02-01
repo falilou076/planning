12c21703ca51954c7efa875ad2eab657306c014f
falilouf076 <falilouf075@gmail.com>
2020-02-01 14:32:08 +0000

    mise en ligne

---
+++ myapp/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+
+# Create your models here.
+
+class Secretaire(models.Model):
+    nom=models.CharField(max_length=50)
+    prenom=models.CharField(max_length=50)
+    mail=models.CharField(max_length=100)
+    telephone=models.CharField(max_length=20)
+    login=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.nom
+
+
+class Classe(models.Model):
+    nom_classe=models.CharField(max_length=50, primary_key=True)
+    password_classe=models.CharField(max_length=50)
+    login_classe = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Classe"
+        ordering = ['nom_classe']
+
+    def __str__(self):
+        return self.nom_classe
+
+
+class Eleve(models.Model):
+    nom_eleve=models.CharField(max_length=50)
+    prenom_eleve=models.CharField(max_length=50)
+    grade_eleve=models.CharField(max_length=40)
+    nom_classe=models.ForeignKey('Classe', on_delete=models.CASCADE)
+
+
+    class Meta:
+        verbose_name = "Eleve"
+        ordering = ['nom_eleve']
+
+    def __str__(self):
+        return self.nom_eleve
+
+class CahierTexte(models.Model):
+    heure_cours=models.CharField(max_length=50)
+    date=models.DateTimeField()
+    contenu=models.TextField()
+    id_eleve=models.ForeignKey('Eleve', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Cahier de texte"
+        ordering = ['date']
+
+    def __str__(self):
+        return self.contenu
+
+
+class Prof(models.Model):
+    nom_prof=models.CharField(max_length=50)
+    prenom_prof=models.CharField(max_length=50)
+    mail_prof=models.CharField(max_length=50)
+    login_prof=models.CharField(max_length=50, primary_key=True)
+    password_prof=models.CharField(max_length=50)
+    grade_prof=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.prenom_prof
+
+
+class Cours(models.Model):
+    nom_cours=models.CharField(max_length=50, primary_key=True)
+    duree=models.CharField(max_length=50)
+    date=models.DateField()
+    login_prof=models.ForeignKey('Prof', on_delete=models.CASCADE)
+    nom_classe = models.ForeignKey('Classe', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nom_cours
+
+class Notification(models.Model):
+    date=models.DateTimeField()
+    contenu=models.TextField()
+    login_prof=models.ForeignKey('Prof', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contenu
+
+class Enseigner(models.Model):
+    nom_classe=models.ForeignKey('Classe', on_delete=models.CASCADE)
+    login_prof=models.ForeignKey('Prof', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nom_classe
+
+
+class PresAbs(models.Model):
+    id_eleve=models.ForeignKey('Eleve', on_delete=models.CASCADE)
+    nom_cours=models.ForeignKey('Cours', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id_eleve
+
+
